629fb60d870a51c32e04e0bea0fd23dda8c1ccab
haidang1810 <99536085+haidang1810@users.noreply.github.com>
2022-03-21 16:49:13 +0700

    Update validator.js

--- validator.js
+++ validator.js
@@ -85,7 +85,30 @@ function validator(formSelector,options){
             }
             if(isValid){
                 if(typeof (options.onSubmit) ==='function'){
-                    options.onSubmit();
+                    var enableInputs = formElement.querySelectorAll('[name]');
+                    var formValues = Array.from(enableInputs).reduce(function (values, input){
+                        switch(input.type){
+                            case 'radio':
+                                values[input.name] = formElement.querySelector(`input[name="${input.name}"]:checked`).value;
+                                break;
+                            case 'checkbox':
+                                if(!input.matches(':checked')) {
+                                    values[input.name] = '';
+                                    return values;
+                                }
+                                if(!Array.isArray(values[input.name])){
+                                    values[input.name] = [];
+                                }
+                                values[input.name].push(input.value);
+                                break;
+                            case 'file':
+                                values[input.name] = input.files;
+                                break;
+                            default: values[input.name] = input.value;
+                        }
+                        return values;
+                    }, {})
+                    options.onSubmit(formValues);
                 }else{
                     formElement.submit();
                 }
